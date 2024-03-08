193710c5942480c9c9e1d3b9a1c3566a4719c762
cpbram2011 <cpbram2011@gmail.com>
2024-03-08 16:22:29 -0600

    fix dropdown onblur

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../reducers/session_reducer';
 import blankPfp from './../assets/blank-pfp.webp'
@@ -7,9 +7,25 @@ import { IoMdArrowDropdown } from "react-icons/io";
 const Navbar = () => {
   const session = useSelector(state => state.session)
   const dispatch = useDispatch()
-  const [dropdown, setDropdown] = useState(false)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [ modalOpen, setModalOpen ] = useState(false)
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        !event.target.classList.contains('dropdown-item')) {
+        setDropdownOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const toggleTheme = () => {
     setIsDarkMode(prevMode => !prevMode);
@@ -21,20 +37,20 @@ const Navbar = () => {
     <div className='navbar'>
       <header>Spotify <span className='mapped'>mapped</span></header>
 
-      {session.isAuthenticated &&
-        <div className='navbar-drop-btn' onClick={() => setDropdown(state => !state)} >
+      { session.isAuthenticated &&
+        <div className='navbar-drop-btn' onClick={() => setDropdownOpen(state => !state)} ref={dropdownRef} >
           <img src={blankPfp} className='pfp' />
           {session.username}
           <IoMdArrowDropdown />
         </div>
       }
-      { dropdown && 
-        <div className='dropdown' >
-          <div className="dropdown-item" onClick={() => setModalOpen(true)}>About</div>
+      { dropdownOpen && 
+        <div className='dropdown'>
+          <div className="dropdown-item" onClick={() => {setModalOpen(true); setDropdownOpen(false)}}>About</div>
           <div className='dropdown-item' onClick={toggleTheme}>
             {!isDarkMode ? 'Light Mode' : 'Dark Mode'}
           </div>
-          <div className="dropdown-item" onClick={() => { dispatch(logout()); setDropdown(false); window.location.href='/' }}>Logout</div>
+          <div className="dropdown-item" onClick={() => { dispatch(logout()); setDropdownOpen(false); window.location.href='/' }}>Logout</div>
         </div>
       }
 
@@ -48,10 +64,10 @@ const Navbar = () => {
           <h1>F.A.Q.</h1> 
             <h3>Is it secure?</h3>
             <p>Statify adheres to all the latest standards in web security. And as logging in to Statify is handled by Spotify, it's as secure as logging in to Spotify itself.</p>
-        
+
             <h3>How is my data used?</h3>
             <p>To use Statify, you'll need to allow the app to have access to some of your account information. Your data is only used to calculate an average with which users can compare their own scores.</p>        
-         
+
             <h3>I have some other issue (or concern, compliment, maybe a suggestion)</h3>
             <a href='mailto:cpbram2011@gmail.com'>Feel free to reach out via email!</a>
               
